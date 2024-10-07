e2c84bed21d2d5ed83ddc8bfa6409d347a6d562f
pricbnll <pricbnll@gmail.com>
2024-10-07 14:09:18 -0300

    feat: fazer base.url e persistir token sem 'Bearer"

--- src/services/ApiUrl.js
+++ src/services/ApiUrl.js
@@ -1,5 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
+const api = axios.create({
+baseURL: 'http://localhost:3000',
 });
+
+api.interceptors.request.use(
+(config) => {
+const token = localStorage.getItem('token');
+
+
+
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+        config.headers['Content-Type'] = 'application/json';
+    }
+    return config;
+},
+(error) => {
+    return Promise.reject(error);
+}
+);
+
+export default api
