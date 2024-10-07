aa43ec6a80506365270aecbf5a2e56bb951a351d
pricbnll <pricbnll@gmail.com>
2024-10-07 15:50:20 -0300

    feat: ApiUrl com baseUrl - token no localstorage

--- src/services/ApiUrl.js
+++ src/services/ApiUrl.js
@@ -1,24 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-baseURL: 'http://localhost:3000',
+  baseURL: "http://localhost:3000",
 });
 
 api.interceptors.request.use(
-(config) => {
-const token = localStorage.getItem('token');
-
-
+  (config) => {
+    const token = localStorage.getItem("token");
 
     if (token) {
-        config.headers.Authorization = `Bearer ${token}`;
-        config.headers['Content-Type'] = 'application/json';
+      config.headers.Authorization = `Bearer ${token}`;
+      config.headers["Content-Type"] = "application/json";
     }
     return config;
-},
-(error) => {
+  },
+  (error) => {
     return Promise.reject(error);
-}
+  }
 );
 
-export default api
+export default api;
