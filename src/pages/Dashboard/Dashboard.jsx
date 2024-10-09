4215d35fa5fae98f1e318e2fc005d7eb75e39b1f
pricbnll <pricbnll@gmail.com>
2024-10-08 23:08:40 -0300

    feat: erro ao buscar lista destinos por id logado

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,85 +1,123 @@
-import { useEffect, useState } from 'react' 
-import '../Dashboard/Dashboard.css'
-import Menu from '../../componentes/Menu/Menu'
-import contaDados from '../../util/contaDados'
-import CardDestino from '../../componentes/CardDestino/CardDestino'
-import Mapa from '../../componentes/Mapa/Mapa'
+import { useEffect, useState } from "react";
+import "../Dashboard/Dashboard.css";
+import Menu from "../../componentes/Menu/Menu";
+import CardDestino from "../../componentes/CardDestino/CardDestino";
+import Mapa from "../../componentes/Mapa/Mapa";
+import api from "../../services/ApiUrl";
 
 function Dashboard() {
-    const [contUsuarios, setContUsuarios] = useState(0)
-    const [usuario, setUsuario] = useState({ nome: ''})
-    const [contDestinos, setContDestinos] = useState(0)
-    const [destinos, setDestinos] = useState([])
-    const [selectedDestino, setSelectedDestino] = useState(null)
-    const [zoomLevel, setZoomLevel] = useState(4)
+  const [usuario, setUsuario] = useState({ nome: "Nome do Viajante", id: null });
+  const [countDestinos, setCountDestinos] = useState(0);
+  const [destinos, setDestinos] = useState([]);
+  const [selectedDestino, setSelectedDestino] = useState(null);
+  const [zoomLevel, setZoomLevel] = useState(4);
 
     useEffect(() => {
-        const usuarioNome = localStorage.getItem('usuarioNome')
-        setUsuario({ nome: usuarioNome })
+        const token = localStorage.getItem("token");
+        console.log(token)
 
-        async function fetchData() {
-            const { contUsuarios, contDestinos } = await contaDados()
-            setContUsuarios(contUsuarios)
-            setContDestinos(contDestinos)
+        // Função para extrair o payload do token JWT
+        const extrairInformacoesDoToken = (token) => {
+            if (!token) return null;
+            const base64Url = token.split('.')[1]; // Pega a segunda parte do token
+            const base64 = window.atob(base64Url); // Decodifica a parte base64 (Payload)
+            return JSON.parse(base64); // Retorna o payload decodificado
+        };
 
-            const response = await fetch('http://localhost:3000/destinos')
-            const data = await response.json()
-            setDestinos(data)
+        const payload = extrairInformacoesDoToken(token);
+        console.log(payload)
+
+        if (payload) {
+            const usuarioNome = payload?.nome || "Nome do Viajante"; //.slice[0]
+            const usuarioId = payload?.sub || null;
+
+            // Define o nome e id do usuário no estado
+            setUsuario({ nome: usuarioNome, id: usuarioId });
         }
 
-        fetchData()
-    }, [])
+    async function fetchData() {
+        try {
+            const usuarioId = usuario.id; 
 
-    const handleMouseEnter = (destino) => {
-        setSelectedDestino(destino)
-        setZoomLevel(4)
+            if (!usuarioId) {
+              console.error("Usuário ID não encontrado");
+              return;
+            }
+            
+            const responseCountDestinos = await api.get(
+              `/destinos/destinos_usuario/${usuarioId}`
+              
+            );
+            const totalDestinoIdData = responseCountDestinos.data.totalDestinoId;
+            setCountDestinos(totalDestinoIdData.count);
+            setDestinos(totalDestinosIdData.rows);
+    
+        } catch (error) {
+            console.error("Erro ao buscar destinos:", error);
+        }
     }
+    fetchData();
+}, [usuario.id]);
 
-    const handleCardClick = (destino) => {
-        setSelectedDestino(destino)
-        setZoomLevel(10)
-    }
+  const handleMouseEnter = (destino) => {
+    setSelectedDestino(destino);
+    setZoomLevel(4);
+  };
 
-    return (
-        <>
-            <div className="dashboard-container">
-                <Menu />
-                <div className="content">
-                    <div className="flex-row first-row">
-                        <div className="flex-column first-column">
-                            <div className="titulo">
-                                <h2>Olá, {usuario.nome}! Bem-vindo(a) ao Birdy!</h2>
-                            </div>
-                            <div className="card">Destinos
-                                <div className="flex-row justify-content-between">
-                                    <img className="icon-card" src="../src/imgs/local-icon.png" alt="Icone Localização" />
-                                    <span className="num-card">{contDestinos}</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="map-container">
-                            <Mapa selectedDestino={selectedDestino} destinos={destinos} zoomLevel={zoomLevel} />
-                        </div>
-                    </div>
-                    <div className="lista-locais">
-                        {destinos.map(destino => (
-                            <CardDestino
-                                key={destino.id}
-                                nome={destino.nome}
-                                descricao={destino.descricao}
-                                cidade={destino.cidade}
-                                estado={destino.estado}
-                                pais={destino.pais}
-                                coordenadas={destino.coordenadas}
-                                onMouseEnter={() => handleMouseEnter(destino)}
-                                onClick={() => handleCardClick(destino)}
-                            />
-                        ))}
-                    </div>
+  const handleCardClick = (destino) => {
+    setSelectedDestino(destino);
+    setZoomLevel(10);
+  };
+
+  return (
+    <>
+      <div className="dashboard-container">
+        <Menu />
+        <div className="content">
+          <div className="flex-row first-row">
+            <div className="flex-column first-column">
+              <div className="titulo">
+                {/* <h2>Olá, {usuarioNome}! Bem-vindo(a) ao Birdy!</h2> */}
+              </div>
+              <div className="card">
+                Destinos
+                <div className="flex-row justify-content-between">
+                  <img
+                    className="icon-card"
+                    src="../src/imgs/local-icon.png"
+                    alt="Icone Localização"
+                  />
+                  <span className="num-card">{countDestinos}</span>
                 </div>
+              </div>
             </div>
-        </>
-    )
+            <div className="map-container">
+              <Mapa
+                selectedDestino={selectedDestino}
+                destinos={destinos}
+                zoomLevel={zoomLevel}
+              />
+            </div>
+          </div>
+          <div className="lista-locais">
+            {destinos.map((destino) => (
+              <CardDestino
+                key={destino.id}
+                nome={destino.nome}
+                descricao={destino.descricao}
+                cidade={destino.cidade}
+                estado={destino.estado}
+                pais={destino.pais}
+                coordenadas={destino.coordenadas}
+                onMouseEnter={() => handleMouseEnter(destino)}
+                onClick={() => handleCardClick(destino)}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
