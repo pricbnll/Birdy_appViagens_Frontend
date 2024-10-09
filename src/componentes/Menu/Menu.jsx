54147401fc9f87592e24c52fb1718bcfdff52886
Fernanda Barros Linhares <fernanda_linhares@estudante.sesisenai.org.br>
2024-10-09 12:07:46 -0300

    Transformando o menu em hamburguer quando mobile

--- src/componentes/Menu/Menu.jsx
+++ src/componentes/Menu/Menu.jsx
@@ -1,45 +1,50 @@
-import '../Menu/Menu.css'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import './Menu.css';
 
 function Menu() {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     const handleLogout = () => {
-        localStorage.removeItem('usuarioNome')
-        localStorage.removeItem('usuarioId')
+        localStorage.removeItem('usuarioNome');
+        localStorage.removeItem('usuarioId');
+        navigate('/');
+    };
 
-        navigate('/')
-    }
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
 
     return (
         <>
-            <nav className='nav-bg'>
-                <div>
+            <nav className={`nav-bg ${isMenuOpen ? 'open' : ''}`}>
+                <div className="menu-header">
                     <img className='logo-img' src="../src/imgs/birdy-icon.png" alt="Logo Birdy" />
                 </div>
-                <div>
-                <Link className='decor-none' to='/dashboard'>
+                <div className={`menu-links ${isMenuOpen ? 'open' : ''}`}>
+                    <Link className='decor-none' to='/dashboard'>
                         <p>Pagina Inicial</p>
                     </Link>
                     <Link className='decor-none' to='/perfil-usuario'>
                         <p>Meu perfil</p>
                     </Link>
-
-                    
                     <Link className='decor-none' to='/locais'>
                         <p>Meus destinos</p>
                     </Link>
                     <Link className='decor-none' to='/cadastro-local'>
                         <p>Novo destino</p>
                     </Link>
-                </div>
-
-                <div>
-                    <p className='decor-none' onClick={handleLogout}><button type="button" className="btn btn-outline-light">Sair</button></p>
+                    <p className='decor-none' onClick={handleLogout}>
+                        <button type="button" className="btn btn-outline-light">Sair</button>
+                    </p>
                 </div>
             </nav>
+            <button className="hamburger-btn" onClick={toggleMenu}>
+                &#9776; 
+            </button>
         </>
-    )
+    );
 }
 
-export default Menu
+export default Menu;
